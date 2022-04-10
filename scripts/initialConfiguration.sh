15224bac87a924bae5f8325a114f37f536e57f68
rakos4 <48767560+rakos4@users.noreply.github.com>
2022-04-10 16:47:35 +0200

    Update initialConfiguration.sh

--- scripts/initialConfiguration.sh
+++ scripts/initialConfiguration.sh
@@ -20,22 +20,14 @@ function configuraAgenteZabbix(){
  cp /usr/share/scripts/zabbix_agentd.conf /etc/zabbix/zabbix_agentd.conf
 }
 
-#funcion que da los permisos necesarios a la clave pública del SSH
-function configuraSSH() {
- chmod 700 /root/.ssh/authorized_keys
- chown root:root /root/.ssh/authorized_keys
-}
-
 #si la configuracion esta realizada se inicia el agente de zabbix y el tomcat, por el contrario, se inicializa la configuración
 if [[ $config == "1" ]]; then
 	service zabbix-agent start
-	service ssh start
         sh bin/catalina.sh run
         
 else
         echo "Iniciando configuración....."
 	configuraTomcat
 	configuraAgenteZabbix
-	configuraSSH
 	echo 1 > /tmp/configuracionCompletada.txt
 fi
